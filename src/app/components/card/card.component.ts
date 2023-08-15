578ebecfdbb940cd94ebafa6cb6db5ec6b2af7aa
facu <facundoduran31@gmail.com>
2023-08-15 17:33:29 -0300

    Boton de borrar ejercicios

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,4 +1,6 @@
 import { Component, Input } from '@angular/core';
+import { EjerciciosService } from 'src/app/services/ejercicios.service';
+import { Ejercicio } from 'src/models/ejercicio';
 
 @Component({
   selector: 'app-card',
@@ -6,6 +8,20 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./card.component.css']
 })
 export class CardComponent {
+
+  constructor(private ejerciciosService: EjerciciosService) { }
+  
+  borrar() {
+    if(this.id != null) {
+      this.ejerciciosService.deleteEjercicios(this.id).subscribe(
+        (response) => console.log(response),
+        (err) => console.log(err)
+      )
+    }
+      
+  }
+
   @Input() titulo: string = '';
   @Input() urlImagen: string = '';
+  @Input() id: number | null = null;
 }
