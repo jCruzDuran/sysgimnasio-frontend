202b38d2f20e321e41c371849ea2f6339ec24107
jCruzDuran <juancruzduran1@gmail.com>
2023-08-17 18:54:13 -0300

    nao nao sirve

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,6 +17,7 @@ import { CardComponent } from './components/card/card.component';
 import { TablaComponent } from './components/tabla/tabla.component';
 import { AddEditPlanesComponent } from './components/add-edit-planes/add-edit-planes.component';
 import { UsuariosComponent } from './components/usuarios/usuarios.component';
+import { DataGridComponent } from './components/data-grid/data-grid.component';
 
 @NgModule({
   declarations: [
@@ -32,7 +33,8 @@ import { UsuariosComponent } from './components/usuarios/usuarios.component';
     CardComponent,
     TablaComponent,
     AddEditPlanesComponent,
-    UsuariosComponent
+    UsuariosComponent,
+    DataGridComponent
   ],
   imports: [
     BrowserModule,
